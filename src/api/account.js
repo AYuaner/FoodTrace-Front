a8c9a31f3be4a4f5af214dcff409853bf36b1c16
AYuaner <1093900650@qq.com>
2022-03-06 16:14:58 +0800

    完成用户管理页面的List、启用、部分new功能

--- src/api/account.js
+++ src/api/account.js
@@ -3,6 +3,22 @@ import request from '@/utils/request'
 export function getList() {
   return request({
     url: '/account/list',
-    method: 'get',
+    method: 'get'
+  })
+}
+
+export function updateEnable(data) {
+  return request({
+    url: '/account/' + data.id,
+    method: 'delete',
+    data: this.datadata
+  })
+}
+
+export function newAccount(data) {
+  return request({
+    url: '/account',
+    method: 'post',
+    data: data
   })
 }
--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="app-container">
+    <el-button type="primary" @click="dialogFormVisible = true">新建用户</el-button>
+    <el-divider />
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="用户名" :label-width="formLabelWidth">
+          <el-input v-model.trim="form.username" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model.trim="form.password" auto-complete="off" />
+        </el-form-item>
+        <el-form-item label="角色" :label-width="formLabelWidth">
+          <el-input v-model.trim="form.role" auto-complete="off" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="cancel">取 消</el-button>
+        <el-button type="primary" @click="commit">确 定</el-button>
+      </div>
+    </el-dialog>
+
     <el-table
       v-loading="listLoading"
       :data="list"
@@ -13,30 +33,27 @@
           {{ scope.$index }}
         </template>
       </el-table-column>
-      <el-table-column label="Title">
+      <el-table-column label="Username" width="200" align="center">
         <template slot-scope="scope">
-          {{ scope.row.title }}
+          {{ scope.row.username }}
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column label="Password" width="200" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
+          <span>{{ scope.row.password }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+      <el-table-column label="Role" width="200" align="center">
         <template slot-scope="scope">
-          {{ scope.row.pageviews }}
+          {{ scope.row.role }}
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
+      <el-table-column label="Status" width="200" align="center">
         <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
+          <el-switch
+            v-model="scope.row.enable"
+            @change="changeEnable(scope.row)"
+          />
         </template>
       </el-table-column>
     </el-table>
@@ -44,7 +61,7 @@
 </template>
 
 <script>
-import { getList } from '@/api/account'
+import { getList, updateEnable, newAccount } from '@/api/account'
 
 export default {
   filters: {
@@ -60,7 +77,14 @@ export default {
   data() {
     return {
       list: null,
-      listLoading: true
+      listLoading: true,
+      dialogFormVisible: false,
+      form: {
+        username: '',
+        password: '',
+        role: ''
+      },
+      formLabelWidth: '120px'
     }
   },
   created() {
@@ -69,9 +93,40 @@ export default {
   methods: {
     fetchData() {
       this.listLoading = true
-      getList().then(response => {
-        this.list = response.data.items
+      getList().then((response) => {
+        this.list = response.data.accounts
         this.listLoading = false
+      })
+    },
+    changeEnable(data) {
+      this.listLoading = true
+      updateEnable(data).then((response) => {
+        if (response.data.result === false) {
+          this.$message('更新状态失败')
+          this.list[data.id].enable = !data.enable
+        }
+        this.listLoading = false
+      })
+    },
+    cancel() {
+      this.form.username = ''
+      this.form.password = ''
+      this.form.role = ''
+      this.dialogFormVisible = false
+    },
+    commit() {
+      newAccount(this.form).then((response) => {
+        if (response.data.result === false) {
+          this.$message('新增用户失败')
+        } else if (response.data.result === true) {
+          this.$message('新增用户成功')
+          this.form.username = ''
+          this.form.password = ''
+          this.form.role = ''
+          this.list = ''
+          this.fetchData()
+        }
+        this.dialogFormVisible = false
       })
     }
   }
@@ -79,4 +134,11 @@ export default {
 
 // TODO 完成account/index相关开发
 
+/*
+TODO
+新建用户时的表单验证
+查询单个用户功能
+编辑用户信息功能
+分页功能
+*/
 </script>
